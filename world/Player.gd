41dd3ee2a725dda1c7d7afbdc82aae269557ac63
Tom Smallridge <sundowns@sundowns-mba.modem>
2022-08-03 23:54:36 +0800

    orient player meshes according to slope

--- world/Player.gd
+++ world/Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 class_name Player
 
 @onready var floor_cast: RayCast3D = $FloorCast
+@onready var mesh_anchor: Node3D = $MeshAnchor
 
 @export var turning_speed: float = 5.0
 
@@ -10,7 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta: float):
 	# Add the gravity.
-	if is_on_floor():
+	if is_on_floor() or floor_cast.is_colliding():
 		grounded_slope_movement(delta)
 	else:
 		aerial_movement(delta)
@@ -20,12 +21,10 @@ func aerial_movement(delta: float):
 	velocity.y -= gravity * delta
 
 func grounded_slope_movement(_delta: float):
-	print('on ground')
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("turn_left", "turn_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, 0)).normalized()
-	print(direction)
 	if direction:
 		velocity.x = direction.x * turning_speed
 	else:
@@ -33,4 +32,15 @@ func grounded_slope_movement(_delta: float):
 	orient_down_slope()
 
 func orient_down_slope():
-	pass
+	if floor_cast.is_colliding():
+		var collision_normal = floor_cast.get_collision_normal()
+		if collision_normal != Vector3.ZERO:
+			var xform = align_with_y(global_transform, collision_normal)
+			mesh_anchor.global_transform = global_transform.interpolate_with(xform, 0.2)
+
+# Given an up direction (y), align the transform so that its local up vector matches y
+func align_with_y(transform: Transform3D, new_y: Vector3):
+	transform.basis.y = new_y
+	transform.basis.x = -transform.basis.z.cross(new_y)
+	transform.basis = transform.basis.orthonormalized()
+	return transform
