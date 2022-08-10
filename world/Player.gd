0e3fc61463e1ae06a397111d38eb18e6b166a480
sundowns <tom@smallridge.com.au>
2022-08-10 22:07:49 +0800

    rotating mesh by edge. need to implement a separate edge system and use that to direct movement

--- world/Player.gd
+++ world/Player.gd
@@ -2,10 +2,10 @@ extends KinematicBody
 class_name Player
 
 onready var floor_cast: RayCast = $FloorCast
+onready var mesh_anchor: Spatial = $MeshAnchor
 
-#export(float) var turning_speed: float = 5.0
 export(float) var forward_speed: float = 10.0
-export(float) var terminal_velocity: float = 100.0
+export(float) var terminal_velocity: float = 50.0
 
 export(Resource) var backside_vector
 export(Resource) var frontside_vector 
@@ -24,14 +24,13 @@ func _physics_process(delta: float):
 	velocity = move_and_slide(velocity, Vector3.UP)
 
 func apply_gravity(delta: float):
-	# Add the gravity.
+	# Add the gravity
 	velocity.y -= gravity * delta
 
 func grounded_slope_movement(delta: float):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var left_right = Input.get_axis("turn_left", "turn_right")
-	var direction = Vector3(left_right, 0, 0).normalized() # local to player / heading
 	# Intentionally dont reset to forward, so we stay on an edge
 	if left_right < 0:
 		heading = backside_vector.xyz
@@ -40,6 +39,7 @@ func grounded_slope_movement(delta: float):
 	var forward_heading_vector = (transform.basis * heading).normalized() * forward_speed * delta
 	velocity += forward_heading_vector
 	orient_down_slope()
+	orient_on_edge(left_right)
 
 func orient_down_slope():
 	if floor_cast.is_colliding():
@@ -47,6 +47,12 @@ func orient_down_slope():
 		if collision_normal != Vector3.ZERO:
 			var xform = align_with_y(global_transform, collision_normal)
 			global_transform = global_transform.interpolate_with(xform, 0.2)
+
+func orient_on_edge(left_right: float):
+	if left_right < 0:
+		mesh_anchor.rotation = Vector3(0,0, 0.1)
+	elif left_right > 0:
+		mesh_anchor.rotation = Vector3(0,0, -0.1)
 
 # Given an up direction (y), align the transform so that its local up vector matches y
 func align_with_y(xform: Transform, new_y: Vector3):
